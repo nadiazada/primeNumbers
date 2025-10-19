53a47736c2fbb69a88d95347338817f992355477
celegarl <celestejolie05@gmail.com>
2025-10-18 19:25:13 -0700

    pushed task 2: range division and thread setup

--- main.cpp
+++ main.cpp
@@ -3,6 +3,12 @@
 #include <vector>
 #include <cstdlib> //to convert raw char into number
 
+// defining a simple struct to hold the thread's number range
+struct Range {
+    unsigned int start;
+    unsigned int end;
+};
+
 // worker_stub is a temporary thread function for Task 1
 //it just runs in each thread and prints a message to confirm threading works
 //later this will handle prime range calculations and file writing
@@ -27,6 +33,29 @@ int main(int argc, char* argv[]) {
  if (numThreads == 0) numThreads = 2;
 
  std::cout << "creating " << numThreads << " threads...\n";
+
+ // dividing range [1...limit] among the threads
+ std::vector<Range> ranges;
+ ranges.reserve(numThreads);
+
+ unsigned int base = limit / numThreads;
+ unsigned int rem = limit % numThreads;
+
+ unsigned int currentStart = 1;
+ for (unsigned int i = 0; i < numThreads; ++i) {
+    unsigned int len = base + ((i < rem) ? 1 : 0);
+    unsigned int currentEnd = currentStart + len - 1;
+    if (currentEnd > limit) currentEnd = limit;
+    ranges.push_back({currentStart, currentEnd});
+    currentStart = currentEnd + 1;
+ }
+
+ // printing the ranges to verify
+ for (unsigned int i = 0; i < ranges.size(); ++i) {
+    std::cout << "Thread " << i
+              << " range: " << ranges[i].start
+              << "-" << ranges[i].end << "\n";
+ }
 
  std::vector<std::thread> threads;
  for (unsigned int i = 0; i < numThreads; ++i) {
